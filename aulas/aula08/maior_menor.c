718715a32ff22d6dcde36fa03b052d707f0a912a
unknown <2512082029@academico.iesb.br>
2025-05-14 10:56:41 -0300

    Adicionado arquivos da aula8

---
+++ aulas/aula08/maior_menor.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+
+    int numeros[10]; // matriz unidimensional = vetor = array = ||||||||||||
+
+    // como accessa cada posição
+    numeros[0] = 5;
+    numeros[1] = 1;
+    numeros[2] = 10;
+    numeros[3] = 6;
+    numeros[4] = -5;
+    numeros[5] = -7;
+    numeros[6] = 7;
+    numeros[7] = 11;
+    numeros[8] = 2;
+    numeros[9] = 0;
+
+    int maior = -99999;
+    int menor = 99999;
+
+
+    printf("Entre com 10 numeros interios:\n");
+    for(int i=0; i<10; i++) {
+        printf("Entre com o numero: ");
+        scanf("%i", &numeros[i]);
+
+        if(maior< numeros[i]){
+            maior = numeros[i];
+        }
+
+        if(menor > numeros[i]){
+            menor = numeros[i];
+        }
+    }
+    
+
+    printf("seus numeros foram: ");
+    for (int i=0; i<10; i++) {
+        printf("%i, ", numeros[i]);
+    }
+
+    
+
+
+
+    printf("\n");
+    printf("o maior numero foi %i e o menor numero foi %i\n", maior, menor);
+
+    return 0;
+    
+}
